3531bd2a97d36658d591ffe9dc13ac18b7f43207
zgira <zgira@10.232.173.152>
2023-10-25 15:46:06 -0500

    studio 6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,9 +12,11 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0) {
 			return 0;
+		} else {
+			return 0.5 + 0.5 * geometricSum(n - 1);
+		}
 		
 	}
 
@@ -27,10 +29,11 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
+		if (p % q == 0) {
+			return q;
+		} else {
+			return gcd(q, p % q);
+		}
 	}
 
 	
@@ -42,10 +45,24 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+		int N = array.length;
+		int[] reversed = new int[N];
+		if (N == 1) {
+			reversed[0] = array[0];
+			return reversed;
+		}
+		return helper(array, reversed, N);
+	}
+	
+	public static int[] helper(int[] arr, int[] newArr, int i) {
+		int x = arr.length;
+		if (i <= (double) x / 2) {
+			return newArr;
+		} else {
+			newArr[x - i] = arr[i - 1];
+			newArr[i - 1] = arr[x - i];
+			return helper(arr, newArr, i - 1);
+		}
 	}
 
 	/**
@@ -59,8 +76,15 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
-		
-		// FIXME
+		if (radius < radiusMinimumDrawingThreshold) {
+			return;
+		} else {
+			StdDraw.circle(xCenter, yCenter, radius);
+			circlesUponCircles(xCenter, yCenter + radius, radius / 3.0, radiusMinimumDrawingThreshold); // Top
+			circlesUponCircles(xCenter, yCenter - radius, radius / 3.0, radiusMinimumDrawingThreshold); // Bottom
+			circlesUponCircles(xCenter - radius, yCenter, radius / 3.0, radiusMinimumDrawingThreshold); // Left
+			circlesUponCircles(xCenter + radius, yCenter, radius / 3.0, radiusMinimumDrawingThreshold); // Right
+		}
 	}
 
 }
